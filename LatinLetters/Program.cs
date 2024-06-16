2d191de6da4a70b48fc8e6b22f20ac5f6612fc1a
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:13:33 +0300

    Initial commit

---
+++ LatinLetters/Program.cs
@@ -0,0 +1,16 @@
+﻿class LatinLetters
+{
+   public static void Main(string[]args)
+   {
+
+       char startLetter = char.Parse(Console.ReadLine());
+       char endLetter = char.Parse(Console.ReadLine());
+
+        
+
+        for (char letter = startLetter; letter <= endLetter; letter++)
+        {
+            Console.Write($"{letter  } ");
+        }
+   }
+   }
